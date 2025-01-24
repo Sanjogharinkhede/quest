59cb5abd90d7b2b973921725fa700de7be21a3bc
sanjog harinkhede <sanjogharinkhede@gmail.com>
2025-01-24 22:54:38 +0530

    Added:Pytest and lambda

---
+++ wipro2/pytst/test_sample_1.py
@@ -0,0 +1,14 @@
+import pytest
+
+def func(x):
+    return x*2
+assert func(5) == 10
+assert func(5) == 11
+
+# def test_func2():
+#     assert func(5) == 10
+#
+# def test_func():
+#     assert func(5) == 11
+
+
